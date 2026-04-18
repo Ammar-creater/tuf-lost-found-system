3c905b13426ac7457c074f93dbce8017f0b252f7
Ammar Maan <ammarmaan64@gmail.com>
2026-04-18 12:35:12 +0500

    Use env variables for admin credentials

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS
+// CORS - Allow frontend from Vercel
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
 
